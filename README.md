169a17f03612dc2e0f7b42793b325020e79a715c
Kyle Cox <kylecox213@gmail.com>
2019-07-28 00:55:28 -0400

    added canvastag.js to the html; uploaded the necessary .js files, added js links

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Student-Portfolio
-New student portfolio
+Student portfolio: customized with css stylings and animated with aos.js and tagcanvas.js
+- Includes links to my:
+    - Github
+    - LinkedIn
+    - COG Systems LLC webpage
+    - Facebook
+    - Resume
+- Also provides users with the capability of sending emails directly
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,6 @@
+
 $(document).ready(function () {
+
     $('.menu-toggler').on('click', function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
@@ -10,21 +12,37 @@ $(document).ready(function () {
     });
 
     $('nav a[href*="#"]').on('click', function () {
-      $('html, body').animate( {
-          scrollTop: $($(this).attr('href')).offset().top - 100
-      }, 2000);
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 2000);
     });
 
     $('#up').on('click', function () {
-        $('html, body').animate( {
-            scrollTop: 0 
+        $('html, body').animate({
+            scrollTop: 0
         }, 2000);
     });
 
     AOS.init({
         easing: 'ease',
         duration: 1800,
-        // once: true
+        once: true
     });
 
+    let options = {
+        textColour: '#05ff11',
+        textHeight: '16',
+        imageAlign: 'centre',
+        shuffleTags: true,
+        noMouse: false,
+        shadow: null,
+        initial: [0.1, 0.2],
+        minSpeed: 0.8,
+        noSelect: true,
+        wheelZoom: true
+    };
+
+    window.onload = function () {
+        TagCanvas.Start('myCanvas', '', options);
+    };
 });
