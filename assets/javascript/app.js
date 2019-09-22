2e8d83e344c66101b6d93b123e1441691b902256
Kyle Cox <kylecox213@gmail.com>
2019-09-22 03:34:44 -0400

    modified styles.css, index.html and app.js; added items to the tag canvas list, modifiying the resume pdf link

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -39,7 +39,7 @@ $(document).ready(function () {
         initial: [0.1, 0.2],
         minSpeed: 0.8,
         noSelect: true,
-        wheelZoom: true
+        wheelZoom: false
     };
 
     window.onload = function () {
